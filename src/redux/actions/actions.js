0cb12d6878aeed9a045c3f93734510b994b27177
Drummer143 <berg.evg.1@gmail.com>
2022-05-18 19:22:38 +0300

    feat: add redux basis

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,7 @@
+export const FETCH_MESSAGES_REQUEST = 'FETCH_MESSAGES_REQUEST';
+export const FETCH_MESSAGES_SUCCESS = 'FETCH_MESSAGES_SUCCESS';
+export const FETCH_MESSAGES_FAILURE = 'FETCH_MESSAGES_FAILURE';
+
+export const signInRequest = () => { type: FETCH_MESSAGES_REQUEST };
+export const signInSuccess = () => { type: FETCH_MESSAGES_SUCCESS };
+export const signInFailure = () => { type: FETCH_MESSAGES_FAILURE };
