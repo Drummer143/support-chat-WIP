33271ad65b8bce08c9cb1efc4cc1bf144df645f8
Drummer143 <berg.evg.1@gmail.com>
2022-06-23 17:36:04 +0300

    feat: add picture uploading and displaying(now you need to reload page to see uploaded image)

--- src/components/chatPart/InputForm/InputForm.js
+++ src/components/chatPart/InputForm/InputForm.js
@@ -1,29 +1,62 @@
-import styles from './InputForm.module.css';
+import { v4 } from 'uuid';
+import { faPaperclip } from '@fortawesome/fontawesome-free-solid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState, useEffect } from 'react';
+import { ref as dRef, update } from 'firebase/database';
+import { ref as sRef, uploadBytes } from 'firebase/storage';
 
-import { ref } from 'firebase/database';
-import { update } from 'firebase/database';
+import { database, storage } from '../../../firebase';
 
-import { database } from '../../../firebase';
+import styles from './InputForm.module.css';
+
+const InputFileButton = ({ setImageInput }) => {
+    return (
+        <div className={styles.imageInput}>
+            <input
+                type='file'
+                id='fileInput'
+                multiple={true}
+                onChange={e => {
+                    console.log(e.target.files[0]);
+                    setImageInput(e.target.files[0]);
+                }
+                }
+                style={{ display: 'none' }}
+            />
+            <button
+                type='button'
+                onClick={() => document.getElementById('fileInput').click()}
+                className={styles.imageButton}
+            ><FontAwesomeIcon icon={faPaperclip} /></button>
+        </div>
+    );
+}
 
 function InputForm({ input, setInput, id, dialogId }) {
     const [localInput, setLocalInput] = useState(input);
+    const [imageInput, setImageInput] = useState(null);
 
     useEffect(() => setLocalInput(input), [input]);
 
-    const headDB = ref(database);
+    const dbRef = dRef(database);
 
+    const uploadImage = () => {
+        const imageRef = sRef(storage, `${dialogId}/${id}/${imageInput.name}_${v4()}`);
+        uploadBytes(imageRef, imageInput)
+            .then(res => console.log(res));
+    }
     const date = new Date();
 
     const sendMessage = () => {
         const message = {
             content: localInput,
+            hasImages: imageInput ? `${dialogId}/${id}/` : '',
             timestamp: date.getTime(),
             writtenBy: 'client'
         };
         let updates = {};
-        updates[`/dialogs/${dialogId}/messages/${id}/`] = message;
-        update(headDB, updates);
+        updates[`/dialogs/${dialogId}/messages/${id}`] = message;
+        update(dbRef, updates);
         /* TODO: update operatorId */
     };
 
@@ -31,24 +64,32 @@ function InputForm({ input, setInput, id, dialogId }) {
         <form
             onSubmit={e => {
                 e.preventDefault();
-                if (localInput) {
+                if (localInput || imageInput) {
                     sendMessage();
+                    setInput('');
                 }
-                setInput('');
+                if (imageInput) {
+                    uploadImage();
+                    setImageInput(null);
+                }
             }}
             onReset={() => setInput('')}
             className={styles.wrapper}
         >
-            <textarea
-                name="input"
-                type="text"
-                /* onChange={formik.handleChange} */
-                onChange={e => setInput(e.target.value)}
-                value={localInput}
-                className={styles.inputField}
-                maxLength={1000}
-            />
+            <div className={styles.inputField}>
+                <textarea
+                    name="input"
+                    type="text"
+                    /* onChange={formik.handleChange} */
+                    onChange={e => setInput(e.target.value)}
+                    value={localInput}
+                    className={styles.textarea}
+                    maxLength={1000}
+                    placeholder='Write a message...'
+                />
 
+                <InputFileButton setImageInput={setImageInput} />
+            </div>
             <div className={styles.buttons}>
                 <button type="reset" className={`${styles.button} ${styles.clear}`}>
                     Clear
@@ -59,6 +100,16 @@ function InputForm({ input, setInput, id, dialogId }) {
                 </button>
             </div>
         </form>
+
+        /* <form
+            onSubmit={e => {
+                e.preventDefault();
+                console.log(imageInput);
+            }}
+        >
+            <InputFileButton setImageInput={setImageInput} />
+            <button type='submit' onClick={uploadImage}>Upload</button>
+        </form> */
     );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
@@ -17,5 +18,6 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const database = getDatabase(app);
+export const storage = getStorage(app);
 
 export const provider = new GoogleAuthProvider();
